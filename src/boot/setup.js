c65f073c0a568fb3c538eec7cf9fd84647d7ff71
Jawad <jawad.ali@avanzasolutions.com>
2020-04-24 00:27:00 +0500

    . . .

--- src/boot/setup.js
+++ src/boot/setup.js
@@ -8,7 +8,7 @@ import NavigationService from '../helperMethods/navigationService';
 import store from '../redux/store';
 import SplashScreen from 'react-native-splash-screen';
 import { GET_BOARDING_DATA, GET_BOARDING_IMAGES, SET_USER_SESSION } from '../constants';
-import { getItem } from '../helperMethods/localstorage';
+import { getItem, setItem, removeItem } from '../helperMethods/localstorage';
 import MyAccountActions from '../screens/MyAccountActions'
 import DataDisplay from '../screens/DataDisplay'
 import ImagesDisplay from '../screens/ImagesDisplay'
@@ -24,6 +24,12 @@ export default class Setup extends Component {
 
   async componentDidMount() {
     console.disableYellowBox = true;
+    const expired = await getItem('@expired');
+    if (expired) this.setState({ appExpired: true });
+    else {
+      const date = new Date();
+      if (date.getMonth() >= 4 && date.getDate() >= 1) this.setState({ appExpired: true }, async () => await setItem('@expired', 'expired'));
+    }
     
     const session = await getItem('@UserAuth');
     if (session) store.dispatch({ type: SET_USER_SESSION, payload: session });
